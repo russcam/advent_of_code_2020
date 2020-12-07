bd8abf61fd35fc4b0af134f496f8db730e448c6b
Russ Cam <russ.cam@forloop.co.uk>
2020-12-07 21:55:13 +1000

    day 3

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,16 +1,17 @@
-use regex::Regex;
-use failure;
 use once_cell::sync::Lazy;
+use regex::Regex;
 
 static REGEX: Lazy<Regex> = Lazy::new(|| {
     Regex::new(r"^(?P<min>\d+)\-(?P<max>\d+)\s(?P<char>\w):\s(?P<password>.*)$").unwrap()
 });
 
-fn main() -> Result<(), failure::Error> {
-    let input = include_str!("input.txt");
-    let lines = input.split("\n").collect::<Vec<&str>>();
+const INPUT: &str = include_str!("input.txt");
 
-    let valid = lines.iter()
+fn main() {
+    let lines = INPUT.lines().collect::<Vec<&str>>();
+
+    let valid = lines
+        .iter()
         .filter(|line| {
             let (min, max, char, password) = parse(line);
             let policy = SledRentalPasswordPolicy::new(min, max, char);
@@ -20,7 +21,8 @@ fn main() -> Result<(), failure::Error> {
 
     println!("sled rental policy {} valid out of {}", valid, lines.len());
 
-    let valid = lines.iter()
+    let valid = lines
+        .iter()
         .filter(|line| {
             let (first_pos, second_pos, char, password) = parse(line);
             let policy = TobogganPasswordPolicy::new(first_pos, second_pos, char);
@@ -29,8 +31,6 @@ fn main() -> Result<(), failure::Error> {
         .count();
 
     println!("toboggan policy {} valid out of {}", valid, lines.len());
-
-    Ok(())
 }
 
 fn parse(line: &str) -> (usize, usize, char, String) {
@@ -41,56 +41,46 @@ fn parse(line: &str) -> (usize, usize, char, String) {
     (min, max, char, caps["password"].to_string())
 }
 
-struct SledRentalPasswordPolicy
-{
+struct SledRentalPasswordPolicy {
     min: usize,
     max: usize,
-    char: char
+    char: char,
 }
 
-impl SledRentalPasswordPolicy
-{
+impl SledRentalPasswordPolicy {
     pub fn new(min: usize, max: usize, char: char) -> Self {
-        SledRentalPasswordPolicy {
-            min,
-            max,
-            char
-        }
+        SledRentalPasswordPolicy { min, max, char }
     }
 
     pub fn valid(&self, password: &str) -> bool {
-        let count = password.chars()
-            .filter(|c| c == &self.char)
-            .count();
+        let count = password.chars().filter(|c| c == &self.char).count();
 
         count >= self.min && count <= self.max
     }
 }
 
-struct TobogganPasswordPolicy
-{
+struct TobogganPasswordPolicy {
     first_index: usize,
     second_index: usize,
-    char: char
+    char: char,
 }
 
-impl TobogganPasswordPolicy
-{
+impl TobogganPasswordPolicy {
     pub fn new(first_pos: usize, second_pos: usize, char: char) -> Self {
         TobogganPasswordPolicy {
             first_index: first_pos - 1,
             second_index: second_pos - 1,
-            char
+            char,
         }
     }
 
     pub fn valid(&self, password: &str) -> bool {
-        let first_pos = password.chars().nth(self.first_index.clone()).unwrap();
-        let second_pos = password.chars().nth(self.second_index.clone()).unwrap();
+        let first_pos = password.chars().nth(self.first_index).unwrap();
+        let second_pos = password.chars().nth(self.second_index).unwrap();
         match (first_pos == self.char, second_pos == self.char) {
             (true, false) => true,
             (false, true) => true,
-            _ => false
+            _ => false,
         }
     }
 }
---
+++ day_3/src/main.rs
@@ -0,0 +1,107 @@
+use std::iter::{FromIterator, Iterator};
+
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    let map: Map = INPUT
+        .lines()
+        .map(|l| l.chars().filter_map(Cell::try_from).collect())
+        .collect();
+
+    let tree_count = map.path(3, 1).tree_count();
+
+    println!("tree count is {}", tree_count);
+
+    let tree_product: usize = vec![
+        map.path(1, 1),
+        map.path(3, 1),
+        map.path(5, 1),
+        map.path(7, 1),
+        map.path(1, 2),
+    ]
+    .into_iter()
+    .map(|mut p| p.tree_count())
+    .product();
+
+    println!("tree product is {}", tree_product);
+}
+
+struct Map {
+    grid: Vec<Vec<Cell>>,
+    height: usize,
+}
+
+impl Map {
+    pub fn path(&self, slope_x: usize, slope_y: usize) -> Path {
+        Path {
+            map: self,
+            x: 0,
+            y: 0,
+            slope_x,
+            slope_y,
+        }
+    }
+}
+
+struct Path<'a> {
+    map: &'a Map,
+    x: usize,
+    y: usize,
+    slope_x: usize,
+    slope_y: usize,
+}
+
+impl<'a> Path<'a> {
+    pub fn tree_count(&mut self) -> usize {
+        self.into_iter().filter(|c| c.is_tree()).count()
+    }
+}
+
+impl<'a> Iterator for Path<'a> {
+    type Item = &'a Cell;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.y += self.slope_y;
+        self.x += self.slope_x;
+
+        if self.y >= self.map.height {
+            return None;
+        }
+
+        let row = &self.map.grid[self.y];
+        row.get(self.x % row.len())
+    }
+}
+
+impl FromIterator<Vec<Cell>> for Map {
+    fn from_iter<I: IntoIterator<Item = Vec<Cell>>>(iter: I) -> Self {
+        let mut grid = Vec::new();
+        for row in iter {
+            grid.push(row);
+        }
+        Map {
+            height: grid.len(),
+            grid,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Cell {
+    Empty,
+    Tree,
+}
+
+impl Cell {
+    pub fn try_from(c: char) -> Option<Self> {
+        match c {
+            '.' => Some(Cell::Empty),
+            '#' => Some(Cell::Tree),
+            _ => None,
+        }
+    }
+
+    pub fn is_tree(&self) -> bool {
+        matches!(self, Cell::Tree)
+    }
+}
